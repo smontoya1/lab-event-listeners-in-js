a6020158794b41e5197611b941715676233c7bc3
Shaun <smontoya0009@gmail.com>
2025-03-16 20:31:30 -0400

    completed lab

--- index.js
+++ index.js
@@ -2,26 +2,26 @@
 
 // Function to change the background color when a button is clicked
 function changeBackgroundColor() {
-  // Implement the function to change background color
+  document.body.style.backgroundColor = 'yellow'
 }
 
 // Function to reset the background color when the body is double-clicked
 function resetBackgroundColor() {
-  // Implement the function to reset background color
+  document.body.style.backgroundColor = 'white'
 }
 
 // Capture Keyboard Input
 
 // Function to display the key pressed by the user
 function displayKeyPress(event) {
-  // Implement the function to display key pressed
+  document.querySelector('#keyPressDisplay').innerText = event.key
 }
 
 // Process Text Input
 
 // Function to display user input in real-time
-function displayUserInput() {
-  // Implement the function to display user input
+function displayUserInput(input) {
+  document.querySelector('#textInputDisplay').innerText = input.target.value
 }
 
 // Attach Event Listeners
